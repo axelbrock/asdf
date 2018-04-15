2c0bcaf972e413507593ae0a386a096d49d92cc8
Axel Brock Cardan <30811286+axelbrock@users.noreply.github.com>
2018-04-14 22:32:41 -0700

    Update innerdep.js

--- innerdep.js
+++ innerdep.js
@@ -1,5 +1,25 @@
-function innerdep (a) {
-  return a + ' hi'
+import React, { Component, PropTypes } from 'react';
+
+class Innerdep extends Component {
+  static PropTypes = {
+    h1: PropTypes.string,
+    h2: PropTypes.string,
+  }
+  render(){
+    let {
+      h2 = 'Hi TL welcome to Custom Modules! :)'
+    } = this.props;
+
+    let {
+      flux, config
+    } = this.context;
+
+    return (
+      <div>
+        <h2>This is InnerDep</h2>
+      </div>
+    );
+  }
 }
 
-export default innerdep
+export default Innerdep;
